aa755d05f6028f7daefe0443797a95ab2c9d00b1
Zach Turner <turnerzdp@gmail.com>
2018-10-25 22:02:39 -0600

    added patterns/ and specs/

--- test/scrape.py
+++ test/scrape.py
@@ -1,29 +1,63 @@
 from bs4 import BeautifulSoup
 from racquet import Racquet
+import os
 
-racquetDataFile = open("asics.txt", "w")
+def scrape(patternFile, outputFile, manufacturer):
+    """Scrape Racquet specs from patternfile and save them in outputFile"""
+    
+    #racquetDataFile = open("asics.txt", "w")
+    racquetDataFile = open(outputFile, "w")
 
-asics_html = open("asics-patterns.html", 'r')
+    #asics_html = open("asics-patterns.html", 'r')
+    patternHTML = open(patternFile, 'r')
 
-soup = BeautifulSoup(asics_html, 'html.parser')
-myracquet = Racquet(1, 2, 3, 4, 5, 6, 7, 8)
+    soup = BeautifulSoup(patternHTML, 'html.parser')
+    #myracquet = Racquet(1, 2, 3, 4, 5, 6, 7, 8)
 
-# All the data we want is in a between <basefont></basefont> tags
-data = soup.find_all('basefont')
-racuqetDataArray = []
-for index in range(len(data)):
-    # The first 2 elemenst of the data array contain information we don't want
-    if(index > 1):
-        racuqetDataArray.append(data[index].contents[0])
+    data = soup.find_all('td')
+    racuqetDataArray = []
+    racquetDataFile.write(str(manufacturer) + " = [[")
+    for index in range(len(data)):
+        # The first 2 elemenst of the data array contain information we don't want
+        if(index > 19):
+            spec = str(data[index]).split("/")
+            spec = spec[1][1:-1]
+            print(spec)
+            racquetDataFile.write("\"")
+            racquetDataFile.write(spec)
+            racquetDataFile.write("\"")
+            if index > len(data) -2:
+                # The last spec is a blank line and we don't want it
+                break
+            if (index - 19) % 8 == 0:
+                # Separate the lists every eight elements
+                print("---")
+                racquetDataFile.write("],[")
+            else:
+                racquetDataFile.write(", ")
 
-print(len(racuqetDataArray))
+    racquetDataFile.write("]]")
 
-racquetArray = []
-index = 0
-while index <= len(racuqetDataArray):
-    currRacquet = Racquet(racuqetDataArray[index], racuqetDataArray[index + 1], racuqetDataArray[index + 2], racuqetDataArray[index + 3], racuqetDataArray[index + 4], racuqetDataArray[index + 5], racuqetDataArray[index + 6], racuqetDataArray[index + 7])
-    racquetArray.append(currRacquet)
-    index += 8
+    racquetDataFile.close()
 
-print(racquetArray)
-#racquetDataFile.close()
+def test():
+    """Test scrape() on asics data"""
+    
+    patterndir = "../patterns"
+    outputdir = "../specs"
+
+    for subdir, dirs, files in os.walk(patterndir):
+        for file in files:
+            #print(file)
+
+            patternFile = "../patterns/Asics Tennis Stringing Patterns.html" #"Babolat Tennis Stringing Patterns.html"
+            #outputFile = "babolat.txt" #"asics.txt"
+            slug = file.split()[0:2]
+            outputFileName = "-".join(slug).lower()
+            #manufacturer = patternFile.split()[0].lower()
+            outputFile = "../specs/" + outputFileName + ".txt"
+
+            scrape(patternFile, outputFile, outputFileName)
+
+if __name__ == '__main__':
+    test()
