4f40b1b446719bfd52510daa4b4e8da2e3fba466
Zach Turner <turnerzdp@gmail.com>
2018-10-25 22:28:14 -0600

    clean up scrape.py

--- test/scrape.py
+++ test/scrape.py
@@ -1,28 +1,24 @@
 from bs4 import BeautifulSoup
-from racquet import Racquet
 import os
 
 def scrape(patternFile, outputFile, manufacturer):
     """Scrape Racquet specs from patternfile and save them in outputFile"""
     
-    #racquetDataFile = open("asics.txt", "w")
     racquetDataFile = open(outputFile, "w")
-
-    #asics_html = open("asics-patterns.html", 'r')
     patternHTML = open(patternFile, 'r')
 
     soup = BeautifulSoup(patternHTML, 'html.parser')
-    #myracquet = Racquet(1, 2, 3, 4, 5, 6, 7, 8)
 
     data = soup.find_all('td')
     racuqetDataArray = []
+    
     racquetDataFile.write(str(manufacturer) + " = [[")
+    
     for index in range(len(data)):
         # The first 2 elemenst of the data array contain information we don't want
         if(index > 19):
             spec = str(data[index]).split("/")
             spec = spec[1][1:-1]
-            # print(spec)
             racquetDataFile.write("\"")
             racquetDataFile.write(spec)
             racquetDataFile.write("\"")
@@ -31,7 +27,6 @@ def scrape(patternFile, outputFile, manufacturer):
                 break
             if (index - 19) % 8 == 0:
                 # Separate the lists every eight elements
-                # print("---")
                 racquetDataFile.write("],[")
             else:
                 racquetDataFile.write(", ")
@@ -48,15 +43,11 @@ def test():
 
     for subdir, dirs, files in os.walk(patterndir):
         for file in files:
-            #print(file)
-
-            patternFile = "../patterns/Asics Tennis Stringing Patterns.html" #"Babolat Tennis Stringing Patterns.html"
-            #outputFile = "babolat.txt" #"asics.txt"
+            patternFile = "../patterns/" + str(file)
             slug = file.split()[0:2]
             outputFileName = "-".join(slug).lower()
-            #manufacturer = patternFile.split()[0].lower()
             outputFile = "../specs/" + outputFileName + ".txt"
-
+            
             print("scraping: " + str(file))
             scrape(patternFile, outputFile, outputFileName)
     print("done!")
