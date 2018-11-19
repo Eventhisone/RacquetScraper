aba69c908e3e0a0084b0fb16d62538757cf09821
Zach Turner <turnerzdp@gmail.com>
2018-11-19 15:12:17 -0700

    Fix .json files

--- test/reformat.py
+++ test/reformat.py
@@ -1,9 +1,10 @@
 import os
 
 def reformat():
-    manufacturer = {}
+    
     for subdir, dirs, files in os.walk("../specs/"):
         for file in files:
+            manufacturer = {}
             fname = "../specs/" + str(file)
             foname = "../specs_json/" + str(file)[:-4] + ".json"
             f = open(fname, "r")
@@ -19,11 +20,12 @@ def reformat():
                 #tie_m = data[5]
                 #start_c = data[6]
                 #tie_c = data[7]
+                print(data[0])
                 racquet_obj = {
                     "name": data[0],
-                    "tension": data[1],
-                    "length": data[2],
-                    "pattern": data[3],
+                    "tension": fix_tension_range(data[1]),
+                    "length": fix_lengths(data[2]),
+                    "pattern": fix_dimensions(data[3]),
                     "skip_m": data[4],
                     "tie_m": data[5],
                     "start_c": data[6],
@@ -33,4 +35,42 @@ def reformat():
             fo.write(str(manufacturer) + "\n")
             f.close()
             fo.close()
-    print(manufacturer)
+    #print(manufacturer)
+
+def fix_tension_range(trange):
+    upper = trange[:2]
+    lower = trange[-2:]
+    
+    tension_range = {
+        "upper": upper,
+        "lower": lower
+    }
+
+    return tension_range
+
+def fix_lengths(lens):
+    #print(lens)
+    length_mains = lens[:2]
+    length_crosses = lens[-4:-2]
+
+    len_mains_crosses = {
+        "length_mains": length_mains,
+        "length_crosses": length_crosses
+    }
+
+    return len_mains_crosses
+
+def fix_dimensions(dims):
+    num_mains = dims[:2]
+    num_crosses = dims[-3:-1]
+
+    dims_mains_and_crosses = {
+        "num_mains": num_mains,
+        "num_crosses": num_crosses
+    }
+
+    return dims_mains_and_crosses
+
+
+if __name__ == '__main__':
+    reformat()
